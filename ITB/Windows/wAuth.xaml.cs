5bfc9641f29400629605a3f3bb03405a8dc1cb15
BriteNight_Cloud <britenight_cloud@mail.ru>
2022-05-15 17:20:02 +0300

    Init Commit
    +Login Window
        Almost done.
        TODO:
        *Sign-In button link to DB
        *User ComboBox link to DB
    
    +Main Window
        TODO:
        *Everything
    
    +Sales Page
        Just a placeholder.

---
+++ ITB/Windows/wAuth.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ITB.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для wAuth.xaml
+    /// </summary>
+    public partial class wAuth : Window
+    {
+        public wAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Drag(object s, MouseButtonEventArgs e) => DragMove();
+
+        private void Exit_Click(object s, RoutedEventArgs e) => Close();
+
+        private void ViewPass(object sender, RoutedEventArgs e)
+        {
+            if (tbPassword.Visibility == Visibility.Collapsed)
+            {
+                tbPassword.Text = pbPassword.Password;
+                tbPassword.Visibility = Visibility.Visible;
+                pbPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                pbPassword.Password = tbPassword.Text;
+                tbPassword.Visibility = Visibility.Collapsed;
+                pbPassword.Visibility = Visibility.Visible;
+            }    
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if(true)
+            {
+                Window w = new wMain();
+                w.Show();
+                Close();
+            }
+        }
+    }
+}
---
+++ ITB/Windows/wMain.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ITB.Pages;
+
+namespace ITB.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для wMain.xaml
+    /// </summary>
+    public partial class wMain : Window
+    {
+        public wMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Menu_Click(object sender, RoutedEventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "General":
+                    frmMain.Navigate(new Page());
+                    break;
+                case "Sales":
+                    frmMain.Navigate(new pSales());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
